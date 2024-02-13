297313b94822fce5c872db3460cc1900bfe96fcb
felix <felixdarielhernandezperez@gmail.com>
2024-02-13 10:46:00 -0500

    Acomodar los ruters

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,36 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+
+import { createBrowserRouter, redirect, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: '/login',
+    element: <h1>Login</h1>,
+  },
+  {
+    path:'/customer',
+    element:<div>Customer</div>,
+    loader:  () => {
+      const user =  localStorage.getItem('userLaPopular')
+      if(!user){
+        console.log(user)
+        return redirect('/login')
+      }
+      return null;
+    }
+  },
+  {
+    path:'/admin',
+    element:<div>Admin</div>
+  }
+]);
+
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
