aa19024ad58a0a5111f8feaeeb3b6ddac6221076
felix <felixdarielhernandezperez@gmail.com>
2024-03-10 10:11:14 -0400

    Solucionado problema en la barra de navegación

--- src/Components/NavbarIcons/Cursor.jsx
+++ src/Components/NavbarIcons/Cursor.jsx
@@ -8,17 +8,17 @@ const Cursor = ({ activeLeft,activeWidth }) => {
 				left: `${activeLeft + activeWidth/2 - 45 / 2}px`, width: '45px', height: '45px',
 				borderRadius: '50%', border: 'solid 5px white', zIndex: 2, transition: 'all 1s ease'
 			}} />
-			<div className='bg-light' style={{position:'absolute',
-			left: `${(activeLeft + activeWidth/2 - 45 / 2)-24/2}px`,top:"20px",transition: 'all 1s ease'
-			,height:'20px',width:'20px'}}>
-				<div className='bg-success' style={{width:'20px',height:'20px',
-				borderRadius:'0px 0px 100px 0px'}}/>
+			<div style={{position:'absolute',
+			left: `${(activeLeft + activeWidth/2 - 45 / 2)-16/2}px`,top:"20px",transition: 'all 1s ease'
+			,height:'20px',width:'10px',backgroundColor:"white"}}>
+				<div className='bg-success' style={{width:'10px',height:'20px',
+				borderRadius:'0px 0px 2emx 0px'}}/>
 			</div>
-			<div className='bg-light' style={{position:'absolute',
-			left: `${(activeLeft + activeWidth/2)+30/2}px`,top:"20px",transition: 'all 1s ease'
-			,height:'20px',width:'20px'}}>
-				<div className='bg-success' style={{width:'20px',height:'20px',
-				borderRadius:'0px 0px 0px 100px'}}/>
+			<div style={{position:'absolute',
+			left: `${(activeLeft + activeWidth/2)+41/2}px`,top:"20px",transition: 'all 1s ease'
+			,height:'20px',width:'10px',backgroundColor:"white"}}>
+				<div className='bg-success' style={{width:'10px',height:'20px',
+				borderRadius:'0px 0px 0px 2emx'}}/>
 			</div>
 		</div>
 
